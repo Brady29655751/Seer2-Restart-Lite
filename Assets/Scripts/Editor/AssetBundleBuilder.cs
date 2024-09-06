6b29af3f939877f9e48bccbd652a4114ede8e195
Fishyet <1241166148@qq.com>
2024-09-06 16:50:28 +0800

    feat:两个小工具,一个可以在动画导入时方便操作预制件,另一个是批量打包PetAnimation的工具(这个只是用来批量命名顺便打包,以后的话直接用打包插件就可以了)

---
+++ Assets/Scripts/Editor/AssetBundleBuilder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace U_Editor
+{
+    using UnityEditor;
+    using UnityEngine;
+    using System.IO;
+
+    public class AssetBundleBuilder//打包精灵动画专用
+    {
+        [MenuItem("Tools/Build PetAnim AssetBundles")]
+        public static void BuildAllAssetBundles()
+        {
+            string assetBundleDirectory = "Assets/AssetBundles";
+            if (!Directory.Exists(assetBundleDirectory))
+            {
+                Directory.CreateDirectory(assetBundleDirectory);
+            }
+
+            string parentFolder = "Assets/PetAnimation"; // Change this to your parent folder path
+            DirectoryInfo dir = new DirectoryInfo(parentFolder);
+            DirectoryInfo[] subDirs = dir.GetDirectories();
+
+            foreach (var subDir in subDirs)
+            {
+                string bundleName = "PFA_" + subDir.Name;
+                FileInfo[] files = subDir.GetFiles("*.prefab", SearchOption.AllDirectories);
+
+                foreach (var file in files)
+                {
+                    string assetPath = file.FullName.Substring(file.FullName.IndexOf("Assets", StringComparison.Ordinal)).Replace("\\", "/");
+                    Debug.Log(Application.dataPath);
+                    Debug.Log(assetPath);
+                    AssetImporter.GetAtPath(assetPath).assetBundleName = bundleName;
+                }
+            }
+
+            BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+        }
+    }
+}
